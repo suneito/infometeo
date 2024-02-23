14abc0a956ba2066675fc5ce5232e3b9a5e239f9
Suneito <39382727+suneito@users.noreply.github.com>
2024-02-23 19:32:43 +0100

    Update ScraperManager.java

--- siscom/src/main/java/cat/babot/scraper/ScraperManager.java
+++ siscom/src/main/java/cat/babot/scraper/ScraperManager.java
@@ -2,16 +2,8 @@ package cat.babot.scraper;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-import org.w3c.dom.NodeList;
-
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class ScraperManager {
     private final Document targetNode;
