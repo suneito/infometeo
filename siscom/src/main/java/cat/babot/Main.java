c1bcbe78b26f9a14feaa8d84497d09f077fb65ad
Suneito <39382727+suneito@users.noreply.github.com>
2024-02-23 18:06:44 +0100

    Update Main.java

--- siscom/src/main/java/cat/babot/Main.java
+++ siscom/src/main/java/cat/babot/Main.java
@@ -2,15 +2,15 @@ package cat.babot;
 
 import cat.babot.blue.Telegram;
 import cat.babot.weather.Localitat;
-import cat.babot.weather.Meteocat;
+//import cat.babot.weather.Meteocat;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.print("Meteocat report");
-        Meteocat meteocat = new Meteocat(Localitat.VIC);
-        meteocat.getWeather();
-        System.out.print(meteocat.simplifyedTodayReport());
+//        Meteocat meteocat = new Meteocat(Localitat.VIC);
+//        meteocat.getWeather();
+//        System.out.print(meteocat.simplifyedTodayReport());
     }
 }
